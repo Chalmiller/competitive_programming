da4f2cce5566a8e2c483d3e9cae32e95f26781da
Chalmiller <chalmiller1@gmail.com>
2020-03-15 14:20:35 -0700

    Initial C++ template

---
+++ cpp/template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+    int a = 123, b = 456;
+    string x = "monkey";
+    cout << a << " " << b << " " << x << "\n";
+      
+    return 0;
+}
