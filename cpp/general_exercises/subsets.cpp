2fd44328ed953c0b6caa4a9bab45b282bfbe4304
Chalmiller <chalmiller1@gmail.com>
2020-03-17 10:22:57 -0700

    Greedy algorithms and complete search

--- cpp/general_exercises/subsets.cpp
+++ cpp/general_exercises/subsets.cpp
@@ -18,6 +18,34 @@ void search(int k) {
     }
 }
 
+void search_permutations() {
+    if (permutation.size() == n) {
+        // process permutation
+    } else {
+        for (int i = 0; i < n; i++) {
+            if (chosen[i]) continue;
+            chosen[i] = true;
+            permutation.push_back(i);
+            search();
+            chosen[i] = false;
+            permutation.pop_back();
+        }
+    }
+}
+
+void backtracking(int y) {
+    if (y == n) {
+        count++;
+        return;
+    }
+    for (int x = 0; x < n; x++) {
+        if (column[x] || diag1[x+y] || diag2[x-y+n-1]) continue;
+        column[x] = diag1[x+y] = diag2[x-y+n-1] = 1;
+        search(y+1);
+        column[x] = diag1[x+y] = diag2[x-y+n-1] = 0;
+    }
+}
+
 int main() {
     return 0;
 }
