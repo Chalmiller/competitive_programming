3296d4cab87c93b7af61ced67e35b575b5de0ed6
Chalmiller <chalmiller1@gmail.com>
2020-03-16 18:09:52 -0700

    Singly and doubly linked lists

---
+++ javascript/data_structures/linked_lists/exercises/reverse_linked_list.js
@@ -0,0 +1,13 @@
+function reverseSingleLinkedList(sll) {
+    var node = sll.head;
+    var prev = null;
+    while(node) {
+        var temp = node.next;
+        node.next = prev;
+        prev = node;
+        if (!temp) 
+        break;
+        node = temp;
+    }
+    return node;
+}
