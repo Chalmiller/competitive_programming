11f1df75040a731ddc3eb32d3f5cd43a12b71b22
Chalmiller <chalmiller1@gmail.com>
2020-03-19 16:03:37 -0700

    python array implementation

---
+++ python/data_structures/arrays/array_basics.py
@@ -0,0 +1,25 @@
+def array_basics():
+
+    # append to the end of a list
+    # arr.append(1)
+
+    # # add a value to the position specified: i => position, x => value
+    # arr.insert(i, x)
+
+    # # pop off and return the last element in a list. Can also specify the position to pop from.
+    # arr.pop()
+
+    # # remove the first occurence of the specified element from the list.
+    # arr.remove(1)
+
+    # # index of the first occurence of the specified value
+    # arr.index(1)
+
+    # # reverse the array. This is probably efficient O(n) time complexity
+    # arr.reverse()
+
+    for i in range(9):
+        print(i)
+
+array_basics()
+
