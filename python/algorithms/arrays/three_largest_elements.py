dc86cfda749517bdbff233ce2d0bfaeb2ac42d35
Chalmiller <chalmiller1@gmail.com>
2020-03-20 10:22:05 -0700

    more algorithms

---
+++ python/algorithms/arrays/three_largest_elements.py
@@ -0,0 +1,16 @@
+def find_three_largest(arr, n):
+    arr = sorted(arr)
+
+    check = 0
+    count = 1
+
+    for i in range(1, n + 1):
+
+        if (count < 4):
+            if (check != arr[n - i]):
+                print(arr[n - i], end = ' ')
+                check = arr[n - i]
+                count += 1
+        else:
+            break
+        
