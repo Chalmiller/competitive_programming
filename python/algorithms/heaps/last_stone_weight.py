f98b20ed535cee65de1f75b56e321bb4facc5492
Chalmiller <chalmiller1@gmail.com>
2020-03-25 13:09:02 -0700

    Heap practice

---
+++ python/algorithms/heaps/last_stone_weight.py
@@ -0,0 +1,25 @@
+# 1046: Last Stone Weight
+
+class Solution:
+    # def lastStoneWeight(self, stones: List[int]) -> int:
+    #     if len(stones) == 1:
+    #         return stones[0]
+    #     else:
+    #         stone_weights = []
+    #         first_max = max(stones)
+    #         stone_weights.append(stones.pop(stones.index(first_max)))
+    #         second_max = max(stones)
+    #         stone_weights.append(stones.pop(stones.index(second_max)))
+    #         if stone_weights[0] == stone_weights[1]:
+    #             stone_weights = []
+    #         else:
+    #             stone_weights = [abs(stone_weights[0] - stone_weights[1])]
+    #         stones.extend(stone_weights)
+    #         return self.lastStoneWeight(stones)
+
+        def lastStoneWeight(self, stones: List[int]) -> int:
+            q = [-stone for stone in stones]
+            heapq.heapify(q)
+            while (len(q)) > 1:
+                heapq.heappush(q, heapq.heappop(q) - heapq.heappop(q))
+            return -q[0]
--- python/data_structures/priority_queue/sorted_priority_queue.py
+++ python/data_structures/priority_queue/sorted_priority_queue.py
@@ -1,21 +1,8 @@
 from .priority_queue_base import PriorityQueueBase
 from ..exceptions import Empty
 
-class UnsortedPriorityQueue(PriorityQueueBase): # base class defines _Item
-  """A min-oriented priority queue implemented with an unsorted list."""
-
-  #----------------------------- nonpublic behavior -----------------------------
-  def _find_min(self):
-    """Return Position of item with minimum key."""
-    if self.is_empty():               # is_empty inherited from base class
-      raise Empty('Priority queue is empty')
-    small = self._data.first()
-    walk = self._data.after(small)
-    while walk is not None:
-      if walk.element() < small.element():
-        small = walk
-      walk = self._data.after(walk)
-    return small
+class SortedPriorityQueue(PriorityQueueBase): # base class defines _Item
+  """A min-oriented priority queue implemented with a sorted list."""
 
   #------------------------------ public behaviors ------------------------------
   def __init__(self):
@@ -28,13 +15,22 @@ class UnsortedPriorityQueue(PriorityQueueBase): # base class defines _Item
 
   def add(self, key, value):
     """Add a key-value pair."""
-    self._data.add_last(self._Item(key, value))
+    newest = self._Item(key, value)             # make new item instance
+    walk = self._data.last()       # walk backward looking for smaller key
+    while walk is not None and newest < walk.element():
+      walk = self._data.before(walk)
+    if walk is None:
+      self._data.add_first(newest)              # new key is smallest
+    else:
+      self._data.add_after(walk, newest)        # newest goes after walk
 
   def min(self):
     """Return but do not remove (k,v) tuple with minimum key.
     Raise Empty exception if empty.
     """
-    p = self._find_min()
+    if self.is_empty():
+      raise Empty('Priority queue is empty.')
+    p = self._data.first()
     item = p.element()
     return (item._key, item._value)
 
@@ -42,6 +38,7 @@ class UnsortedPriorityQueue(PriorityQueueBase): # base class defines _Item
     """Remove and return (k,v) tuple with minimum key.
     Raise Empty exception if empty.
     """
-    p = self._find_min()
-    item = self._data.delete(p)
+    if self.is_empty():
+      raise Empty('Priority queue is empty.')
+    item = self._data.delete(self._data.first())
     return (item._key, item._value)
