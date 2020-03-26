d094dd4d6dbbce255800a69c1425c56b75558a34
Chalmiller <chalmiller1@gmail.com>
2020-03-26 15:42:13 -0700

    More hashmap problems

---
+++ python/algorithms/hash_map/n-repeated_element.py
@@ -0,0 +1,10 @@
+from typing import *
+from collections import Counter
+
+class Solution:
+    def repeatedNTimes(self, A: List[int]) -> int:
+        counter = Counter()
+        for num in A:
+            counter[num] += 1
+        return counter.most_common(1)[1]
+        
