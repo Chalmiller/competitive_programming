d9731e158cadfb0aa1d83a65ff441b22266ac2dd
Chalmiller <chalmiller1@gmail.com>
2020-03-26 16:03:03 -0700

    more hashmap practice - this time with collections, which are awesome

--- python/algorithms/hash_map/unique_num_occurence.py
+++ python/algorithms/hash_map/unique_num_occurence.py
@@ -1,18 +1,22 @@
+# 1207: unique Number of Ocurrences
 from typing import *
 from collections import Counter
 
 class Solution:
-    def repeatedNTimes(self, A: List[int]) -> int:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
         counter = Counter()
         new_set = set()
-        for num in A:
+        for num in arr:
             counter[num] += 1
 
-        for el,k in dict(counter):
+        for el,k in counter.items():
             if k in new_set:
                 return False
             else:
                 new_set.add(k)
                 
         return True
-        
+
+obj = Solution()
+num = obj.uniqueOccurrences([1,2,2,1,1,3])
+print(num)
