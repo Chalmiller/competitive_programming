33515e7737f41a0e82f13a58668a967c0074c0b1
Chalmiller <chalmiller1@gmail.com>
2020-03-30 17:02:53 -0700

    depth frist search

---
+++ python/algorithms/trees/leaf_similar_trees.py
@@ -0,0 +1,19 @@
+# 872: LeafSimilar Trees
+from typing import *
+import itertools
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def leafSimilar(self, root1: TreeNode, root2: TreeNode) -> bool:
+        def dfs(node):
+            if not node: return
+            if not node.left and not node.right: yield node.val
+            for i in dfs(node.left): yield i
+            for i in dfs(node.right): yield i
+        return all(a == b for a, b in itertools.izip_longest(dfs(root1), dfs(root2)))
