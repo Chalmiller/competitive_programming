7daff02880199b1dba139719f6e259bb752a6e44
Chalmiller <chalmiller1@gmail.com>
2020-04-09 11:05:47 -0700

    Two Pointer and three pointer technique

--- python/algorithms/two_pointer/pair_with_target_sum.py
+++ python/algorithms/two_pointer/pair_with_target_sum.py
@@ -1,12 +1,24 @@
 def pair_with_targetsum(arr, target_sum):
-  left, right = 0, len(arr) - 1
-  while(left < right):
-    current_sum = arr[left] + arr[right]
-    if current_sum == target_sum:
-      return [left, right]
+  #  Two Pointer Approach
 
-    if target_sum > current_sum:
-      left += 1  # we need a pair with a bigger sum
+  # left, right = 0, len(arr) - 1
+  # while(left < right):
+  #   current_sum = arr[left] + arr[right]
+  #   if current_sum == target_sum:
+  #     return [left, right]
+
+  #   if target_sum > current_sum:
+  #     left += 1  # we need a pair with a bigger sum
+  #   else:
+  #     right -= 1  # we need a pair with a smaller sum
+  # return [-1, -1]
+
+  # Hash Table Approach (Dynamic Programming)
+  num_hash = {}
+  for num in arr:
+    num_needed = target_sum - num
+    if num_needed in num_hash:
+      return [num, num_needed]
     else:
-      right -= 1  # we need a pair with a smaller sum
-  return [-1, -1]
+      num_hash.update(num)
+  return [-1,-1]
---
+++ python/algorithms/two_pointer/squared_sorted_array.py
@@ -0,0 +1,3 @@
+def make_squares(arr):
+  squares = sorted(list(map(lambda a: a*a, arr)))
+  return squares
