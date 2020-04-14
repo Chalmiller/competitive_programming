cb184bbd7b8fb9a6fa636707813f64398dd16537
Chalmiller <chalmiller1@gmail.com>
2020-04-14 11:44:42 -0700

    min_depth_tree.py

--- python/algorithms/trees/BFS/min_depth_binary_tree.py
+++ python/algorithms/trees/BFS/min_depth_binary_tree.py
@@ -7,40 +7,41 @@ class TreeNode:
     self.left, self.right = None, None
 
 
-def find_level_averages(root):
-  result = []
+def find_minimum_depth(root):
   if root is None:
-    return result
+    return 0
 
   queue = deque()
   queue.append(root)
+  minimumTreeDepth = 0
   while queue:
+    minimumTreeDepth += 1
     levelSize = len(queue)
-    levelSum = 0.0
     for _ in range(levelSize):
       currentNode = queue.popleft()
-      # add the node's value to the running sum
-      levelSum += currentNode.val
-      # insert the children of current node to the queue
+
+      # check if this is a leaf node
+      if not currentNode.left and not currentNode.right:
+        return minimumTreeDepth
+
+      # insert the children of current node in the queue
       if currentNode.left:
         queue.append(currentNode.left)
       if currentNode.right:
         queue.append(currentNode.right)
-    # append the current level's average to the result array
-    result.append(levelSum / levelSize)
-    
-  return result
+
 
 
 def main():
   root = TreeNode(12)
   root.left = TreeNode(7)
   root.right = TreeNode(1)
-  root.left.left = TreeNode(9)
-  root.left.right = TreeNode(2)
   root.right.left = TreeNode(10)
   root.right.right = TreeNode(5)
-  print("Level averages are: " + str(find_level_averages(root)))
+  print("Tree Minimum Depth: " + str(find_minimum_depth(root)))
+  root.left.left = TreeNode(9)
+  root.right.left.left = TreeNode(11)
+  print("Tree Minimum Depth: " + str(find_minimum_depth(root)))
 
 
 main()
