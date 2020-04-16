3d0e554e6ade738ad2ee76e6ade24c6581402894
Chalmiller <chalmiller1@gmail.com>
2020-04-16 12:03:47 -0700

    bfs algorithm

--- python/algorithms/trees/BFS/level_order_traversal_2.py
+++ python/algorithms/trees/BFS/level_order_traversal_2.py
@@ -9,4 +9,26 @@ from collections import deque
 
 class Solution:
     def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        queue = deque()
+        if root is None:
+          return root
+
+        queue.append(root)
+        result = []
+        while queue:
+          curr_length = len(queue)
+          current_level = []
+          for _ in range(curr_length):
+            curr_el = queue.popleft()
+            current_level.append(curr_el.val)
+
+            if curr_el.left:
+              queue.append(curr_el.left)
+            if curr_el.right:
+              queue.append(curr_el.right)
+            
+          result.insert(0, current_level)
         
+        return result
+
+
