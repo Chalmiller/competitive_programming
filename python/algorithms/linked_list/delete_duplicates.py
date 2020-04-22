30bfa2ddb5b8ae6b446a4e9733572954b28c88ed
Chalmiller <chalmiller1@gmail.com>
2020-04-21 18:38:33 -0700

    More linked list practice

---
+++ python/algorithms/linked_list/delete_duplicates.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        curr = head
+        while curr.next:
+          if curr.val == curr.next.val:
+            curr.next = curr.next.next
+          curr = curr.next
+
+        
