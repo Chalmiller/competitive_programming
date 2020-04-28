a87fd1c77c04fd412c69f94cc2b9c930dd682b60
Chalmiller <chalmiller1@gmail.com>
2020-04-27 17:47:46 -0700

    maximal matrix

---
+++ python/algorithms/matrix/maximal_square.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maximalSquare(self, matrix):
+      if not matrix: return 0
+      m , n = len(matrix),len(matrix[0])
+      dp = [[0 if matrix[i][j]=='0' else 1 for j in range(n)]for i in range(m)]
+      
+      for i in range(1,m):
+          for j in range(1,n):
+              if matrix[i][j] =='1': dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
+              else: dp[i][j] = 0
+      
+      ans = max([max(i) for i in dp])
+      return ans ** 2
+
+obj = Solution()
+num = obj.maximalSquare([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]])
+print(num)
