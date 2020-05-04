59e3a4795c5bc9fb9d5a16a2a8bf7411c0b7d486
Chalmiller <chalmiller1@gmail.com>
2020-05-04 11:32:25 -0700

    subset problem

---
+++ python/algorithms/subsets/evaluate_expression.py
@@ -0,0 +1,20 @@
+def diff_ways_to_evaluate_expression(input):
+  result = []
+
+  if '+' not in input and '-' not in input and '*' not in input:
+    result.append(int(input))
+  else:
+    for i in range(0, len(input)):
+      char = input[i]
+      if not char.isdigit():
+        leftParks = diff_ways_to_evaluate_expression(input[0:i])
+        rightParts = diff_ways_to_evaluate_expression(input[i+1:])
+        for part1 in leftParks:
+          for part2 in rightParts:
+            if char == '+':
+              result.append(part1 + part2)
+            elif char == '-':
+              result.append(part1 - part2)
+            elif char == '*':
+              result.append(part1 * part2)
+  return result
