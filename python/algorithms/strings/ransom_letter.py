bb4ad2ace7d160296f3915af7833d4a088fbfac6
Chalmiller <chalmiller1@gmail.com>
2020-05-03 17:48:48 -0700

    some heap practice

---
+++ python/algorithms/strings/ransom_letter.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        mag_inv = dict()
+        
+        for let in magazine:
+            if let not in mag_inv:
+                mag_inv[let] = 0
+            mag_inv[let] += 1
+        
+        print(mag_inv)
+        while ransomNote:
+            let = ransomNote[0]
+            if let in mag_inv and mag_inv[let] > 0:
+                mag_inv[let] -= 1
+                ransomNote = ransomNote[1:]
+            else:
+                return False
+            print(ransomNote, mag_inv)
+        return True
+
+obj = Solution()
+print(obj.canConstruct("aa", "aab"))
