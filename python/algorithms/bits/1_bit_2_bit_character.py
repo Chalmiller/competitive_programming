0da635c0b70ac5c390cf3da3579536b9f2fab5b3
Chalmiller <chalmiller1@gmail.com>
2020-05-15 12:52:29 -0700

    what an odd question

---
+++ python/algorithms/bits/1_bit_2_bit_character.py
@@ -0,0 +1,8 @@
+from typing import *
+
+class Solution(object):
+    def isOneBitCharacter(self, bits):
+        i = 0
+        while i < len(bits) - 1:
+            i += bits[i] + 1
+        return i == len(bits) - 1
