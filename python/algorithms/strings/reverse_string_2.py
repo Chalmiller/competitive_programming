5f4a8af8864f5fea1cd4c1cffbf89bcc2c5c18f7
Chalmiller <chalmiller1@gmail.com>
2020-05-16 19:33:12 -0700

    okay

--- python/algorithms/strings/reverse_string_2.py
+++ python/algorithms/strings/reverse_string_2.py
@@ -4,7 +4,7 @@ class Solution:
     def reverseStr(self, s: str, k: int) -> str:
         for i in range(0, len(s), 2*k):
           s = "".join(list(reversed(s[i:i+k])))
-          print(s)
+          print(s) 
 
 obj = Solution()
 print(obj.reverseStr("abcdefg", 2))
