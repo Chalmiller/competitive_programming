0d0f4a41f10a8cf8e66354d3f880397119ef050c
Chalmiller <chalmiller1@gmail.com>
2020-06-02 16:48:12 -0700

    more training

--- python/algorithms/graphs/all_paths_from_source_to_target.py
+++ python/algorithms/graphs/all_paths_from_source_to_target.py
@@ -1,5 +1,29 @@
 from typing import *
+import collections
 
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        
+        """
+        1. generate an adjacency list
+        2. dfs search
+        3. append path to response list
+        3. return response list once all nodes are visited
+        """
+        paths = []
+        path = []
+        n = len(graph)
+
+        def dfs(cur):
+          nonlocal paths
+          nonlocal path
+          path.append(cur)
+
+          if cur == n - 1:
+            paths.append(path[:])
+          else:
+            for adj in graph[cur]:
+              dfs(adj)
+          path.pop()
+
+        dfs(0)
+        return paths
