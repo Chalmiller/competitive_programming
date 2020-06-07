86fe90b379ad941664c77ffd4a9a9602d361dc92
Chalmiller <chalmiller1@gmail.com>
2020-06-07 10:54:22 -0700

    daily training

---
+++ python/educative.io/data_structures_for_coding_interviews_in_python/graph_bfs.py
@@ -0,0 +1,8 @@
+def bfs_traversal(g, source):
+    result = ""
+    num_of_vertices = g.vertices
+    """
+    
+    """
+    # For the above graph, it should return "01234" or "02143" etc
+    return result
