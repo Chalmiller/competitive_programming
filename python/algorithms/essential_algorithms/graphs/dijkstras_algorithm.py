d38726e867bf660889a5ec5770bdde35f44cf763
Chalmiller <chalmiller1@gmail.com>
2020-06-08 09:41:24 -0700

    daily training

--- python/algorithms/essential_algorithms/graphs/dijkstras_algorithm.py
+++ python/algorithms/essential_algorithms/graphs/dijkstras_algorithm.py
@@ -1,35 +1,59 @@
 import sys
 
-class Graph():
+# Function to find out which of the unvisited node 
+# needs to be visited next
+def to_be_visited():
+  global visited_and_distance
+  v = -10
+  # Choosing the vertex with the minimum distance
+  for index in range(number_of_vertices):
+    if visited_and_distance[index][0] == 0 \
+      and (v < 0 or visited_and_distance[index][1] <= \
+      visited_and_distance[v][1]):
+        v = index
+  return v
 
-  def __init__(self, vertices):
-    self.v = vertices
-    self.graph = [[0 for column in range(vertices)] for row in range(vertices)]
+# Creating the graph as an adjacency matrix
+vertices = [[0, 1, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+            [0, 0, 0, 0]]
+edges =  [[0, 3, 4, 0],
+          [0, 0, 0.5, 0],
+          [0, 0, 0, 1],
+          [0, 0, 0, 0]]
 
-  def printSolution(self, dist):
-    print("Vertex \tDistance from Source")
-    for node in range(self.V):
-      print(node, '\t', dist[node])
+number_of_vertices = len(vertices[0])
 
-  def minDistance(self, dist, sptSet):
-    min = sys.maxint
-    for v in range(self.V):
-      if dist[v] < min and sptSet[v] == False:
-        min = dist[v]
-        min_index = v
-    return min_index
+# The first element of the lists inside visited_and_distance 
+# denotes if the vertex has been visited.
+# The second element of the lists inside the visited_and_distance 
+# denotes the distance from the source.
+visited_and_distance = [[0, 0]]
+for i in range(number_of_vertices-1):
+  visited_and_distance.append([0, sys.maxsize])
 
-  def dijkstra(self, src):
+for vertex in range(number_of_vertices):
+  # Finding the next vertex to be visited.
+  to_visit = to_be_visited()
+  for neighbor_index in range(number_of_vertices):
+    # Calculating the new distance for all unvisited neighbours
+    # of the chosen vertex.
+    if vertices[to_visit][neighbor_index] == 1 and \
+     visited_and_distance[neighbor_index][0] == 0:
+      new_distance = visited_and_distance[to_visit][1] \
+      + edges[to_visit][neighbor_index]
+      # Updating the distance of the neighbor if its current distance
+      # is greater than the distance that has just been calculated
+      if visited_and_distance[neighbor_index][1] > new_distance:
+        visited_and_distance[neighbor_index][1] = new_distance
+    # Visiting the vertex found earlier
+    visited_and_distance[to_visit][0] = 1
 
-    dist = [sys.maxint] * self.v
-    dist[src] = 0
-    sptSet = [False] * self.v
+i = 0 
 
-    for count in range(self.v):
-      u = self.minDistance(dist, sptSet)
-      sptSet[u] = True
-
-      for v in range(self.v):
-        if (self.graph[u][v] > 0 and sptSet[v] == False and \
-          dist[v] > dist[u] + self.graph[u][v]):
-          dist[v] = dist[u] + self.graph[u][v]
+# Printing out the shortest distance from the source to each vertex       
+for distance in visited_and_distance:
+  print("The shortest distance of ",chr(ord('a') + i),\
+  " from the source vertex a is:",distance[1])
+  i = i + 1
