22d6220dab16ff842268d5015cef2edcd0b11a97
Chalmiller <chalmiller1@gmail.com>
2020-06-19 12:54:54 -0700

    more greedy practice

---
+++ python/interview_cake/greedy/max_profit.py
@@ -0,0 +1,65 @@
+import math
+
+def get_max_profit(stock_prices):
+  """
+  Task: Find an optimal buy/sell time that would maximize profit
+
+  Algorithm:
+  1. greedy for loop taking the min value and at each step determining max profit
+  """
+  if len(stock_prices) < 2:
+      raise ValueError("Getting a prodit requires at least 2 prices.")
+      
+  _min = stock_prices[0]
+  profit = stock_prices[1] - stock_prices[0]
+  
+  for time in range(1, len(stock_prices)):
+    current_price = stock_prices[time]
+    potential_profit = current_price - _min
+    
+    profit = max(profit, potential_profit)
+    _min = min(_min, current_price)
+    
+  return profit
+
+
+# Tests
+import unittest
+
+class Test(unittest.TestCase):
+
+    def test_price_goes_up_then_down(self):
+        actual = get_max_profit([1, 5, 3, 2])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_down_then_up(self):
+        actual = get_max_profit([7, 2, 8, 9])
+        expected = 7
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_up_all_day(self):
+        actual = get_max_profit([1, 6, 7, 9])
+        expected = 8
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_down_all_day(self):
+        actual = get_max_profit([9, 7, 4, 1])
+        expected = -2
+        self.assertEqual(actual, expected)
+
+    def test_price_stays_the_same_all_day(self):
+        actual = get_max_profit([1, 1, 1, 1])
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_error_with_empty_prices(self):
+        with self.assertRaises(Exception):
+            get_max_profit([])
+
+    def test_error_with_one_price(self):
+        with self.assertRaises(Exception):
+            get_max_profit([1])
+
+
+unittest.main(verbosity=2)
