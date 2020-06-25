dcbddd85776829780e3dbc70b162477336b1130f
Chalmiller <chalmiller1@gmail.com>
2020-06-25 12:06:20 -0700

    daily training

--- python/algorithms/trees/kth_smallest_iterative_approach.py
+++ python/algorithms/trees/kth_smallest_iterative_approach.py
@@ -1,4 +1,22 @@
 from typing import *
+import collections
 
 class Solution:
     def kthSmallest(self, root: TreeNode, k: int) -> int:
+      curr = root
+      stack = collections.deque()
+      count = 0
+
+      while True:
+        while curr:
+          stack.append(curr)
+          curr = curr.left
+        
+        if stack:
+          curr = stack.pop()
+          count += 1
+          if count == k:
+            return curr.val
+          curr = curr.right
+        else:
+          return -1
