84cf7761d08e6be7a74a9546d8a9ad894aa15fd9
Chalmiller <chalmiller1@gmail.com>
2020-06-26 16:35:45 -0700

    more practice

---
+++ python/leetcode_core_patterns/arrays/max_subarray.py
@@ -0,0 +1,15 @@
+from typing import *
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+      if not nums:
+        return nums
+
+      max_here = nums[0]
+      max_so_far = nums[0]
+
+      for i in range(1, len(nums)):
+        max_here = max(nums[i], max_here + nums[i])
+        max_so_far = max(max_so_far, max_here)
+      return max_so_far
+
