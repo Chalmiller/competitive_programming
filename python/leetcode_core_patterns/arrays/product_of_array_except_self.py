93ea3660fc2d5166244dcbf2220f8f656c883a2e
Chalmiller <chalmiller1@gmail.com>
2020-06-26 12:33:55 -0700

    more practice

---
+++ python/leetcode_core_patterns/arrays/product_of_array_except_self.py
@@ -0,0 +1,12 @@
+class Solution:
+  def productExceptSelf(self, nums: List[int]) -> List[int]:
+    """
+    Task: Return an array that has its elements as the product of all arrays except itself
+
+    Algorithm:
+      1. 
+    """
+    # product of the array to the left
+    left_prod = []
+    for i in range(len(nums)-1, -1, -1):
+      left_prod.append()
