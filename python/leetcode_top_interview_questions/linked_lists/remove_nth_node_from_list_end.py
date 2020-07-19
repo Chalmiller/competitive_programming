c98656db137b98ceee505d8329dd86f02fb2ce67
Chalmiller <chalmiller1@gmail.com>
2020-07-18 20:09:31 -0700

    more training

---
+++ python/leetcode_top_interview_questions/linked_lists/remove_nth_node_from_list_end.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        # two pointer solution
+        ptr1, ptr2 = 0, 0
+        slow, fast = head, head
+
+        while fast.next and fast.next.next:
+          slow = slow.next
+          fast = fast.next.next
+          ptr1 += 1
+          ptr2 += 2
+        
+        # the length of the list will be equal to fast
+        while ptr1 < (ptr2 + 1 - n):
+          slow = slow.next
+          ptr1 += 1
+
+        slow.next = slow.next.next
+        
+
+        
