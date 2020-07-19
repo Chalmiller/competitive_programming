0ac156749dc3e1c091dc1291fd6f055d4d59f800
Chalmiller <chalmiller1@gmail.com>
2020-07-19 10:43:43 -0700

    daily training

--- python/leetcode_core_patterns/strings/longest_palindromic_substring.py
+++ python/leetcode_core_patterns/strings/longest_palindromic_substring.py
@@ -1,4 +1,4 @@
-from typing import *
+import unittest
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
@@ -8,5 +8,28 @@ class Solution:
 
         Algorithm: 
           1. have one pointer at the end and one at the beginning
-          2. 
+          2. slice each step
         """
+        if len(s) < 3:
+            if s[::-1] == s:
+                return s
+            else:
+                return s[0]
+        max_length = ""
+        for i in range(len(s) - 1):
+            for j in range(i+1, len(s)):
+                # O(n)
+                new_slice = s[i:j+1]
+                if new_slice == new_slice[::-1]:
+                    if len(new_slice) > len(max_length):
+                        max_length = new_slice
+        return max_length if len(max_length) > 1 else s[0]
+            
+class TestSolution(unittest.TestCase):
+    
+    def is_one_character(self):
+        a = "a"
+        self.assertTrue(Solution().longestPalindrome(a))
+        
+if __name__ == "__main__":
+    unittest.main()
