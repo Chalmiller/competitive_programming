d5e4792ebfc47c750e46b4b1f81162ce6469ce0f
Chalmiller <chalmiller1@gmail.com>
2020-08-02 15:34:19 -0700

    daily training

--- python/leetcode_top_interview_questions/trees/validate_binary_search_tree.py
+++ python/leetcode_top_interview_questions/trees/validate_binary_search_tree.py
@@ -1,4 +1,5 @@
-import math
+from typing import *
+import unittest
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -9,45 +10,13 @@ import math
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
         """
-        Task: 
-          - return boolean if the tree is a valid binary search tree
-        Intuition:
-          - left will be less than the node and right will be more than the node
-
-        Algorithm:
-          - we can use dfs on the tree
-          - if left is less than, run recursion on the left 
-          - if right is greater than node, run recursion on the right side
-          - return if either of those conditions don't hold up
+        Task: Verify that the binary search tree is a valid BST
+        Intuition: Preorder traversal will 
         """
 
-        # def helper(node, lower = -math.inf, upper = math.inf):
-        #   if not node:
-        #     return True
-        #   val = node.val
-        #   if val <= lower or val >= upper:
-        #     return False
-          
-        #   if not helper(node.right, val, upper):
-        #     return False
-        #   if not helper(node.left, lower, val):
-        #     return False
-        #   return True
+class TestIsValidBST(unittest.TestCase):
 
-        # return helper(root)
+    def test_valid_tree(self):
+      pass
 
-        stack, inorder = [], 0
-
-        while stack or root:
-          while root:
-            stack.append(root.val)
-            root = root.left
-          
-          root = stack.pop()
-
-          if root.val <= inorder:
-            return False
-          inorder = root.val
-          root = root.right
-
-        return True
+unittest.main(verbosity=2)
