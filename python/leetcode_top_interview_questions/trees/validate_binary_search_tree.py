cbcb0395c3764b58e9cf6ca7d7472f1de8785cb4
Chalmiller <chalmiller1@gmail.com>
2020-08-02 17:37:53 -0700

    more daily training

--- python/leetcode_top_interview_questions/trees/validate_binary_search_tree.py
+++ python/leetcode_top_interview_questions/trees/validate_binary_search_tree.py
@@ -1,5 +1,6 @@
 from typing import *
 import unittest
+import math
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -11,8 +12,46 @@ class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
         """
         Task: Verify that the binary search tree is a valid BST
-        Intuition: Preorder traversal will 
+        Intuition: Preorder traversal will append the nodes in least to greatest order.
+
+        Algorithm:
+          - Do in order traversal of the tree and append each node to a closure list
+          - iterate through that array and check that every element is in place
         """
+
+        # def inorder_dfs(node, inorder_nodes):
+        #   if not node:
+        #     return 
+        #   else:
+        #     inorder_dfs(node.left, inorder_nodes)
+        #     inorder_nodes.append(node.val)
+        #     inorder_dfs(node.right, inorder_nodes)
+        
+        # inorder_list = []
+        # inorder_dfs(root, inorder_list)
+
+        # for i in range(len(inorder_list) - 1):
+        #   if inorder_list[i] > inorder_list[i+1]:
+        #     return False
+
+        # return True
+
+        stack = []
+        inorder = -math.inf
+
+        while stack or root:
+          while root:
+            stack.append(root)
+            root = root.left
+
+          root = stack.pop()
+
+          if root.val <= inorder:
+            return False
+          inorder = root.val
+          root = root.right
+        return True
+
 
 class TestIsValidBST(unittest.TestCase):
 
