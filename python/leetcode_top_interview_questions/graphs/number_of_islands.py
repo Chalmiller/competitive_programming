9b81da1afd266a91d0a7790535b73b6ab39d50f3
Chalmiller <chalmiller1@gmail.com>
2020-08-04 16:11:52 -0700

    more training

---
+++ python/leetcode_top_interview_questions/graphs/number_of_islands.py
@@ -0,0 +1,28 @@
+from typing import *
+import unittest
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        self.ROWS = len(grid) - 1
+        self.COLS = len(grid[0]) - 1
+
+        def consume(row, col, grid):
+            if row < 0 or row > self.ROWS or col < 0 or col > self.COLS  or grid[row][col] == '0':
+                return grid
+            else:
+                grid[row][col] = '0'
+                consume(row+1, col, grid); consume(row-1, col, grid)
+                consume(row, col + 1, grid); consume(row, col-1, grid)
+
+        num_islands = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == '1':
+                    grid = consume(row, col, grid)
+                    num_islands += 1
+
+class TestNumIslands(unittest.TestCase):
+    def test_num_of_islands(self):
+        pass
+
+unittest.main(verbosity=2)
