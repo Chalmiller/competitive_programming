ba05598a38a0da3380af0308758bf1e5470aa9d6
Chalmiller <chalmiller1@gmail.com>
2020-08-06 11:33:02 -0700

    daily training

---
+++ python/algorithms/trees/delete_node_in_tree.py
@@ -0,0 +1,18 @@
+from typing import *
+import unittest
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        pass
+
+class TestDeleteNode(unittest.TestCase):
+    def test_delete_node(self):
+        pass
+
+unittest.main(verbosity=2)    
