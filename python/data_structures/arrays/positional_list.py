3294906abdd6d78b1915ea965495dd420b969a7d
Chalmiller <chalmiller1@gmail.com>
2020-08-16 18:27:19 -0700

    changes

--- python/data_structures/arrays/positional_list.py
+++ python/data_structures/arrays/positional_list.py
@@ -112,3 +112,17 @@ class PositionalList(_DoublyLinkedBase):
     original._element = e               # replace with new element
     return old_value                    # return the old element value
     
+def insertion_sort(L):
+  if len(L) > 1:
+    marker = L.first()
+    while marker != L.last():
+      pivot = L.after(marker)
+      value = pivot.element()
+      if value > marker.element():
+        marker = pivot
+      else:
+        walk = marker
+        while walk != L.first() and L.before(walk).element() > value:
+          walk = L.before(walk)
+        L.delete(pivot)
+        L.add_before(walk, value)
